3009b993d0db50c8cc2ef7939d5237813e7f0c1f
晓生 王 <aherosir@gmail.com>
2023-08-10 08:49:07 +0000

    0810

--- Buildroot/package/partclone/partclone.mk
+++ Buildroot/package/partclone/partclone.mk
@@ -14,6 +14,15 @@ PARTCLONE_CONF_OPTS = --enable-static --enable-xfs --enable-btrfs --enable-ntfs 
 PARTCLONE_EXTRA_LIBS = -ldl
 PARTCLONE_CONF_ENV += LIBS="$(PARTCLONE_EXTRA_LIBS)"
 
+# define PARTCLONE_AUTORECONF_CMDS
+#     $(AUTORECONF) $(AUTORECONF_OPTS)
+#     $(SED) -i -e 's/Partclone/UPP/g' /workspace/fos/fssourcex64/output/build/partclone-0.3.20/src/partclone.c
+#     $(SED) -i -e 's/http:\/\/partclone.org/https:\/\/upp.ltd/g' /workspace/fos/fssourcex64/output/build/partclone-0.3.20/src/partclone.c
+# 	$(EACH) $(BUILD_DIR)
+# endef
+
+# PARTCLONE_POST_PATCH_HOOKS += PARTCLONE_AUTORECONF_CMDS
+
 define PARTCLONE_LINK_LIBRARIES_TOOL
 	ln -f -s $(BUILD_DIR)/xfsprogs-*/include/xfs $(STAGING_DIR)/usr/include/
 	ln -f -s $(BUILD_DIR)/xfsprogs-*/libxfs/.libs/libxfs.* $(STAGING_DIR)/usr/lib/
